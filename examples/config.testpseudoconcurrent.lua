01b89a288e01f1973063b8a0e3a289b0004a3dc0
JTrancender <mkeyboard814@gmail.com>
2022-10-27 11:05:21 +0800

    feat: add skynet pseudo concurrent (#9)

---
+++ examples/config.testpseudoconcurrent.lua
@@ -0,0 +1,16 @@
+root = "./"
+
+DEBUG = true
+project_name = "examples"
+thread = 8
+harbor = 0
+start = "testpseudoconcurrent"
+bootstrap = "snlua bootstrap"
+luaservice = root.."service/?.lua;"..root.."skynet/service/?.lua;"..root.."service/?/main.lua;"..root..project_name.."/?.lua"
+lualoader = root.."skynet/lualib/loader.lua"
+cpath = root.."skynet/cservice/?.so;"..root.."cservice/?.so"
+lua_cpath = root.."luaclib/?.so;"..root.."skynet/luaclib/?.so"
+lua_path = root.."lualib/?.lua;"..root..project_name.."/?.lua;"..root.."skynet/lualib/?.lua;"..root.."lualib/?/init.lua;"..root.."skynet/lualib/skynet/?.lua"
+snax = root.."service/?.lua;"..root.."skynet/service/?.lua"
+
+preload = "./lualib/swt/debug.lua"
